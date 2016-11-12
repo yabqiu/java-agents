14fce86c1677e2959472d72d5908b0badfd05bf0
Yanbin <yabqiu@gmail.com>
2016-11-11 23:59:58 -0600

    modified README file

--- jdk1.5/TransClass.java
+++ jdk1.5/TransClass.java
@@ -1,6 +1,6 @@
 public class TransClass {
   public int getNumber(){
-    // return 1;  //compile to file TransClass.class
-   return 2;  //compile to file TransClass.class.2
+    return 1;  //compile to file TransClass.class
+  //  return 2;  //compile to file TransClass.class.2
   }
 }
